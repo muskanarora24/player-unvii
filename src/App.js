6831752cb9ee13c9f8a6645fea7a2f8117eb6977
Muskan Arora <aroramuskan499@gmail.com>
2024-06-03 16:32:05 +0530

    change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { useParams } from 'react-router-dom';
 import VideoPlayer from './components/VideoPlayer';
 
 function App() {
-  const { '*':videoUrl } = useParams();
+  let { '*':videoUrl } = useParams();
+  // For types of parts in URL: http and https
+  if (videoUrl.includes("http:/"))
+    videoUrl = videoUrl.replace("http:/", "http://");
+  if (videoUrl.includes("https:/"))
+    videoUrl = videoUrl.replace("https:/", "https://");
+
   console.log(videoUrl)
   const decodedVideoUrl = decodeURIComponent(videoUrl);
   console.log(decodedVideoUrl)
